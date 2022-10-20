c64313a8265c7e330c0b797b53e5bfb4dafe31b5
Lacika <vargalacii111@gmail.com>
2022-10-20 14:33:57 +0200

    Add project files.

---
+++ Caracter/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Karakterfigyelő
+{
+    private char _Char;
+
+    public void Ellenőriz(char c)
+    {
+        if (c == _Char) RosszKarakter(EventArgs.Empty);
+        _Char = c;
+    }
+
+    private void RosszKarakter(EventArgs e)
+    {
+        EventHandler kezelő = RosszKarakterHandler;
+        if (kezelő != null) kezelő(this, e);
+    }
+
+    public event EventHandler RosszKarakterHandler;
+}
+
+internal static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Várom a szöveget...");
+        Karakterfigyelő figyelo = new Karakterfigyelő();
+        char c;
+        figyelo.RosszKarakterHandler += RosszKarakterr;
+        do
+        {
+            c = Console.ReadKey().KeyChar;
+            figyelo.Ellenőriz(c);
+        } while (c != (int)ConsoleKey.Enter);
+    }
+
+    private static void RosszKarakterr(object sender, EventArgs e)
+    {
+        Console.WriteLine("\nRossz karakter!");
+    }
+}
